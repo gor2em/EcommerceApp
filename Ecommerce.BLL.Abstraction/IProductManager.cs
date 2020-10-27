ad72554bb8008674fc59700e16365662cf22b69f
Görkem Acar <grkemacar@outlook.com>
2020-10-27 14:56:56 +0200

    - IProductManager eklendi

---
+++ Ecommerce.BLL.Abstraction/IProductManager.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Models.DTO;
+using Ecommerce.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.BLL.Abstraction
+{
+    public interface IProductManager:IManager<Product>
+    {
+        ICollection<Product> Search(ProductSearchCriteriaDTO dto);
+        ICollection<Product> GetShopId(int shopId);
+    }
+}
