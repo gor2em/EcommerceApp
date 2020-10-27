e4d5da80204e59e81e81ec8e46f41d6b5ab85671
Görkem Acar <grkemacar@outlook.com>
2020-10-27 15:15:55 +0200

    ShopManager eklendi

---
+++ Ecommerce.BLL/ShopManager.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.BLL.Abstraction.Base;
+using Ecommerce.BLL.Abstraction.Contracts;
+using Ecommerce.Models.EntityModels;
+using Ecommerce.Repository.Abstraction.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.BLL
+{
+    public class ShopManager : Manager<Shop>, IShopManager
+    {
+        private IShopRepository _shopRepository;
+        public ShopManager(IShopRepository shopRepository) : base(shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+
+        public bool Add(Shop entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadProduct(Shop shop)
+        {
+            _shopRepository.LoadProduct(shop);
+        }
+    }
+}
