5fa867c43f04bd2e9605a7bc63df61a6496c0284
Görkem Acar <grkemacar@outlook.com>
2020-10-27 15:07:25 +0200

    - CustomerManager dahil edildi

---
+++ Ecommerce.BLL/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.BLL.Abstraction.Base;
+using Ecommerce.BLL.Abstraction.Contracts;
+using Ecommerce.Models.EntityModels;
+using Ecommerce.Repository.Abstraction.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.BLL
+{
+    public class CustomerManager : Manager<Customer>, ICustomerManager
+    {
+        private ICustomerRepository _customerRepository;
+        public CustomerManager(ICustomerRepository customerRepository) : base(customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public bool Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
